a0261147f84f520cda981f10a16e758b32ab4e3c
ahcorde <ahcorde@gmail.com>
2020-06-05 16:33:45 +0200

    Remove PLUGINLIB_EXPORT_CLASS from header
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- ign-rviz-plugins/include/ignition/rviz/plugins/tf_display.hpp
+++ ign-rviz-plugins/include/ignition/rviz/plugins/tf_display.hpp
@@ -65,9 +65,4 @@ private:
 }  // namespace plugins
 }  // namespace rviz
 }  // namespace ignition
-
-#include <pluginlib/class_list_macros.hpp>  // NOLINT
-PLUGINLIB_EXPORT_CLASS(ignition::rviz::plugins::TFDisplay,
-                       ignition::rviz::plugins::MessageDisplayBase)
-
 #endif  // IGNITION__RVIZ__PLUGINS__TF_DISPLAY_HPP_
