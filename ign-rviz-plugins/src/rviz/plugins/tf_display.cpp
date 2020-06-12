0e971e33c79b161a4863fec95c30578746108c18
Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>
2020-06-13 03:16:15 +0530

    Update copyright info
    Signed-off-by: Sarathkrishnan Ramesh <sarathkrishnan99@gmail.com>

--- ign-rviz-plugins/src/rviz/plugins/tf_display.cpp
+++ ign-rviz-plugins/src/rviz/plugins/tf_display.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2020 Sarathkrishnan Ramesh
+// Copyright (c) 2020 Open Source Robotics Foundation, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -23,7 +23,6 @@
 #include <utility>
 #include <memory>
 #include <vector>
-
 
 namespace ignition
 {
